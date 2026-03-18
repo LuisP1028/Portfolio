5707f1311b8028d2a19a9d4bb5d9d9b54ebf1e18
ChoppedCheese <luisp1.0280@gmail.com>
2026-03-17 17:28:50 -0700

    d

--- js/chat-widget.js
+++ js/chat-widget.js
@@ -22,7 +22,7 @@ window.initializeChatLogic = function() {
 
     const followHTML = `
         <div id="follow-module" class="follow-module">
-            <span class="follow-label">[SYS_LINKS] >></span>
+            <span class="follow-label">[FOLLOW PLATODOOM] >></span>
             
             <a href="https://x.com/PLATODOOM" target="_blank" class="follow-link">
                 <svg class="social-icon" viewBox="0 0 24 24" fill="currentColor"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
